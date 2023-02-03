20f0213c1076af8d696482db5acd4f80532cd757
Akari Bowens <akarimbowens@gmail.com>
2023-02-03 15:00:48 -0800

    Adding Tip Toggle Button and MUI dependencies

---
+++ Todo.md
@@ -0,0 +1,12 @@
+1. add form
+2. turn into material ui
+
+- tip amount
+- bill split slider
+- total bill input
+
+3. Add useState Bill total
+4. add display component
+5. make colors mor visible
+6. test on mobile
+7. put on hosting site
--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  createTheme,
+  FormControl,
+  Input,
+  Slider,
+  ThemeProvider,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
+// import { FormGroup } from "@material-ui/core";
+import { useState } from "react";
 
 function App() {
+  const [tipAmount, setTipAmount] = useState("");
+
+  const handleTip = (event, newTipAmount) => {
+    setTipAmount(newTipAmount);
+  };
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#93E9BE",
+        darker: "#47D990",
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <FormControl>
+          <ToggleButtonGroup
+            aria-label="Tip Amount"
+            color="primary"
+            exclusive
+            value={tipAmount}
+            onChange={handleTip}
+          >
+            <ToggleButton
+              variant="contained"
+              value="fifteenPercentTip"
+              aria-label="fifteen percent tip"
+            >
+              15%
+            </ToggleButton>
+            <ToggleButton
+              value="eighteenPercentTip"
+              aria-label="eighteen percent tip"
+            >
+              18%
+            </ToggleButton>
+            <ToggleButton
+              value="twentyPercentTip"
+              aria-label="twenty percent tip"
+            >
+              20%
+            </ToggleButton>
+            <ToggleButton value="customPercentTip" aria-label="custom tip">
+              Custom
+            </ToggleButton>
+          </ToggleButtonGroup>
+          <Slider
+            defaultValue={1}
+            step={1}
+            min={1}
+            max={4}
+            valueLabelDisplay="auto"
+            aria-labelledby="billSplitSlider"
+          />
+          {/* <Input aria-labelledby="billSplitSlider" /> */}
+        </FormControl>
+      </ThemeProvider>
     </div>
   );
 }
